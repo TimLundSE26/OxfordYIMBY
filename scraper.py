c489b68fde1f630b198011fc147321e01d209927
TimLundSE26 <TimLundSE26@users.noreply.github.com>
2017-12-14 18:44:40 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,8 +9,6 @@ month = "Oct 17"
 def search(mth):
 	
   	request_data = {"month": mth, "dateType": "DC_Validated" , "searchType": "Application" }
-
-
 	sleep(2)
 	result = requests.post('http://public.oxford.gov.uk/online-applications/monthlyListResults.do?action=firstPage', request_data)
 	
@@ -18,18 +16,25 @@ def search(mth):
 		print "No result returned"
 		return
 	
-	result_dom = fromstring(result.content)
-  
-  	results = result_dom.xpath("//li[@class='searchresult']")
-  	print len(results)
-	
-	iPage = 2
-	request_data = {"searchCriteria.page": "2", "action": "page", "orderBy": "DateReceived", "orderByDirection": "Ascending", "searchCriteria.resultsPerPage": "10"}
-	
-	sleep(2)
-	result = requests.post('http://public.oxford.gov.uk/online-applications/pagedSearchResults.do', request_data)
 	result_dom = fromstring(result.content)  
   	results = result_dom.xpath("//li[@class='searchresult']")
   	print len(results)
+	
+	for result in results:
+		link = "".join(result.xpath('a/@href')).strip()
+		description = "".join(result.xpath('a//text()')).strip()
+		address = "".join(result.xpath('p[@class="address"]/text()')).strip()
+		meta = "".join(result.xpath('p[@class="metaInfo"]/text()')).strip()
+		
+		print link, meta
+		
+#	iPage = 2
+#	request_data = {"searchCriteria.page": "2", "action": "page", "orderBy": "DateReceived", "orderByDirection": "Ascending", "searchCriteria.resultsPerPage": "10"}
+	
+#	sleep(2)
+#	result = requests.post('http://public.oxford.gov.uk/online-applications/pagedSearchResults.do', request_data)
+#	result_dom = fromstring(result.content)  
+ # 	results = result_dom.xpath("//li[@class='searchresult']")
+  #	print len(results)
 	
 search(month)
